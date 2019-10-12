aabf2a266168b5de63404f3b56b3eb3a087c5304
Yan Brovkin <brovkin.yan@gmail.com>
2019-10-12 10:45:22 +0200

    feat: add Persistence layer to actor

---
+++ ConsoleApp/Domain/ShardEnvelope.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp.Domain
+{
+    public sealed class ShardEnvelope
+    {
+        public readonly int ShardId;
+        public readonly int EntityId;
+        public readonly object Message;
+        public ShardEnvelope(int shardId, int entityId, object message)
+        {
+            this.ShardId = shardId;
+            this.EntityId = entityId;
+            this.Message = message;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Topshelf;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.Service<ChatService>(s =>
+                {
+                    s.ConstructUsing(service => new ChatService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+
+                windowsService.RunAsLocalSystem();
+                windowsService.StartAutomatically();
+
+                windowsService.SetDescription("TopshelfDotNetCoreExample");
+                windowsService.SetDisplayName("TopshelfDotNetCoreExample");
+                windowsService.SetServiceName("TopshelfDotNetCoreExample");
+            });
+        }
+    }
+}
